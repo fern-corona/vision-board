a138dc52dfba5bbebb2d9631ce39eea363798162
Fernanda Corona <fernanda@Mac.lan>
2026-05-18 22:18:21 -0700

    backend changes for deployment

--- backend/core/config.py
+++ backend/core/config.py
@@ -3,4 +3,5 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-DATABASE_URL = os.getenv("DATABASE_URL")
+# DATABASE_URL = os.getenv("DATABASE_URL") TODO: DEV
+DATABASE_URL = os.environ["DATABASE_URL"]
